4ccded4d921ff295166bd5bf8f3fab145e40b0f2
Orel Chalfon <orelchalfon12@gmail.com>
2024-04-24 17:30:33 +0300

    basically first section with img and some about me title and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { motion } from 'framer-motion'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+import Logo2 from './assets/images/Logo2.jpg'
+function App()
+{
 
   return (
     <>
       <div dir='rtl'>
-        <h1>NatiBarberShop</h1>
-        <p >ברוכים הבאים ל- <span>NatiBarberShop</span></p>
-     </div>
+        <header className='container'>
+
+
+          <motion.img
+            initial={{ scale: 1.1, y: '-100vw' }}
+            animate={{ y: 0 }}
+            transition={{ type: 'spring', delay: 0.5 }}
+            whileHover={{ scale: [1.1, 1, 1.1, 1] }}
+
+
+            src={Logo2} alt="Logo2" className="logo" />
+          <div className="content">
+            <motion.h1
+
+              initial={{ y: '-100vw' }}
+              animate={{ y: 0 }}
+              transition={{ type: 'spring', }}
+            >Welcome To NatiBarberShop</motion.h1>
+
+            <motion.p initial={{ x: '-100vw' }}
+              animate={{ x: 0 }}
+              transition={{ type: 'spring', delay: 0.5 }}
+            >
+              שלום שמי[שם החבר] ואני מספר עם חמש שנות ניסיון בתחום .אני מתמחה בתספורות גברים וילדים במהלך השנים צברתי נסיון עם מגוון רחב של לקוחות. במספרה שלי ,אנו מציעים חוויה מקצועית ואישית ,תוך שימת דגש על הבנת צרכי הלקוח והתאמת התספורת המושלמת לכל אחד ואחתץ אני מאמין שתסםורת מעולה יכולה לשפר משמעותית את הביטחון העצמי ולהפוך את היום שלך לטוב יותר כאן,ב[שם המספרה] אנו מחוייבים לאיכות ושירות ללא פשרות ,כל זאת באווירה חמה ומזמינה .אני מזמין אתכם להגיע ולחוות את ההבדל בהצמכם !
+            </motion.p>
+          </div>
+
+        </header>
+        {/* <section className='container'>asd</section>
+        <footer className='container'>asd</footer> */}
+      </div>
     </>
   )
 }
---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../../assets/images/"
+const Carousel = () => {
+  return (
+    <div>Carousel</div>
+  )
+}
+
+export default Carousel
